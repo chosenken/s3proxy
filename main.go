6e21b4639a124458f0cc84c5059daf744e8a0590
Ken Herner <ken@astronomer.io>
2017-11-10 17:49:58 -0500

    Updates s3proxy to redirect to a presigned URL
    Signed-off-by: Ken Herner <ken@astronomer.io>

--- main.go
+++ main.go
@@ -1,18 +1,15 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"net/http"
-	"strconv"
-	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
+	"time"
 )
 
 var (
@@ -37,43 +34,28 @@ func listenAndServe() {
 }
 
 func getS3File(ctx *gin.Context) {
-	logger := log.WithField("function", "getS3File")
 	// File should be in the format of "/<bucket name>/<key to file>"
 	bucket := ctx.Param("bucket")
 	key := ctx.Param("key")
-	keySplit := strings.SplitAfter(key, "/")
-	fileName := keySplit[len(keySplit)-1]
 	if len(bucket) == 0 {
 		ctx.AbortWithStatusJSON(http.StatusBadRequest, gin.H{"message": "S3 File must be specified"})
 		return
 	}
-	numBytes, buff, err := downloadS3File(&bucket, &key, ctx)
+	sess, err := session.NewSession()
 	if err != nil {
-		logger.Error(err)
-		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
+		return
 	}
-	ctx.Header("Content-Description", "File Transfer")
-	ctx.Header("Content-Transfer-Encoding", "binary")
-	ctx.Header("Content-Disposition", "attachment; filename="+fileName)
-	ctx.Header("Content-Length", strconv.FormatInt(numBytes, 10))
-	logger.WithFields(logrus.Fields{"fileName": fileName, "numBytes": numBytes}).Debug("Returning file")
-	ctx.Data(http.StatusOK, "application/octet-stream", buff)
-}
+	s3svc := s3.New(sess)
+	req, _ := s3svc.GetObjectRequest(&s3.GetObjectInput{
+		Bucket: aws.String(bucket),
+		Key: aws.String(key),
+	})
 
-func downloadS3File(bucket, key *string, ctx context.Context) (int64, []byte, error) {
-	logger := log.WithField("function", "downloadS3File")
-	logger.WithFields(logrus.Fields{"bucket": bucket, "key": key}).Debug("Downloading S3 File")
-	s3Downloader := s3manager.NewDownloader(session.New(&aws.Config{
-		Region: aws.String("us-east-1"),
-	}))
-	buff := &aws.WriteAtBuffer{}
-	input := &s3.GetObjectInput{
-		Bucket: bucket,
-		Key:    key,
-	}
-	numBytes, err := s3Downloader.DownloadWithContext(ctx, buff, input)
+	url, err := req.Presign(5 * time.Minute)
 	if err != nil {
-		return 0, nil, err
+		ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{"error":err.Error()})
 	}
-	return numBytes, buff.Bytes(), nil
+
+	ctx.Redirect(http.StatusTemporaryRedirect, url)
 }
